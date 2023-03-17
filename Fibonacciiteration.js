a47d8785e013bafc0b123ccd40983a9ad367a768
raghujk365 <128111107+raghujk365@users.noreply.github.com>
2023-03-17 11:59:31 +0530

    Create Fibonacciiteration.js

---
+++ Fibonacciiteration.js
@@ -0,0 +1,24 @@
+function fibonacci(n) {
+  let fibSeries = [];
+
+  if (n === 0) {
+    return [0];
+  } else if (n === 1) {
+    return [0, 1];
+  } else {
+    fibSeries = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+      let nextFib = fibSeries[i - 1] + fibSeries[i - 2];
+      fibSeries[i] = nextFib;
+    }
+
+    return fibSeries;
+  }
+}
+
+
+console.log(fibonacci(0)); 
+console.log(fibonacci(1)); 
+console.log(fibonacci(5));
+console.log(fibonacci(10));
