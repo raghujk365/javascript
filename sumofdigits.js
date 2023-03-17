53c062aac73126581956fe983f54cc1f2ee26d17
raghujk365 <128111107+raghujk365@users.noreply.github.com>
2023-03-17 09:11:40 +0530

    Create sumofdigits.js

---
+++ sumofdigits.js
@@ -0,0 +1,13 @@
+function sumofDigits(number) {
+let sum = 0;
+while (number) {
+sum += number%10;
+number = Math.floor(number/10);
+ 
+ }
+ return sum;
+ }
+ const number = 12345;
+ const sum = sumOfDigits(number);
+ console.log(`The sum of digits in ${number} is ${sum}. `);
+ 
